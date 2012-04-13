dbce80eba81270a86c3037e49217437d1f7ba7ad
Andrew Crump <andrew@kotirisoftware.com>
2012-04-13 21:32:35 +0100

    Increment version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "peterc@xley.com"
 license          "Apache 2.0"
 description      "Installs/Configures Trac"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.3"
 depends          "apache2"
